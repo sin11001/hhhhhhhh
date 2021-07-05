7344beb490eb1e43aec53a9e59d9b46a62e2a090
sin11001 <85130599+sin11001@users.noreply.github.com>
2021-07-05 21:00:12 +0530

    Update Card.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ca.sheridancollege.week3.softwarefundamentals.ice1;hhhhvhgfcchcgnfchctthg
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
  * A class that models playing card Objects. Cards have 
